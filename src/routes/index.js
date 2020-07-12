1e36885f17899be278c0569ade9bb38bfeae50d3
Sporanzi <arnaud.cavat@gmail.com>
2020-07-13 01:17:17 +0200

    plug elos route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 const players = require('./players');
 const leagues = require('./leagues');
 const matches = require('./matches');
+const elos = require('./elos');
 
 module.exports = {
   '/players': players,
   '/matches': matches,
   '/leagues': leagues,
+  '/elos': elos,
 };
