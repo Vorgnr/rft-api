a7dd7d2c59ec8bd212a349b2ef1ddec5ecb1eb5a
Sporanzi <arnaud.cavat@gmail.com>
2020-07-15 11:33:23 +0200

    Allow front

--- server.js
+++ server.js
@@ -13,12 +13,21 @@ dotenv.config();
 
 const port = process.env.PORT;
 const env = process.env.NODE_ENV;
+const frontApp = process.env.FRONT_APP;
 
 module.exports = {
   start: async () => {
     debug('Server preparing %s using port %d...', env, port);
     const { knex, Controllers } = await setup(config.database[env]);
     debug('%d Controllers ok ...', Object.keys(Controllers).length);
+
+    app.use((req, res, next) => {
+      res.setHeader('Access-Control-Allow-Origin', frontApp);
+      res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+      res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+      res.setHeader('Access-Control-Allow-Credentials', true);
+      next();
+    });
 
     app.use(bodyParser.urlencoded());
     app.use(bodyParser.json());
